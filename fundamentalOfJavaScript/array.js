2f31542434219236b85681bfbafe4109f738c775
m-abirhasan23 <abirhasanjoy07@gmail.com>
2025-09-29 23:24:48 +0600

    array of everything

---
+++ fundamentalOfJavaScript/array.js
@@ -0,0 +1,11 @@
+const matA = [
+  [1, 2, 4],
+  [8, 16, 64],
+  [288, 456, 899],
+];
+
+for (let i = 0; i < matA.length; i++) {
+  for (let j = 0; j < matA[i].length; j++) {
+    console.log(matA[i][j]);
+  }
+}
